7a67ea2e47cc62435de9a8165abed10ccb41b69d
mit-goti <mitgoti3@gmail.com>
2018-04-14 17:24:07 +0530

    ___

--- app/src/main/java/com/example/meetgoti/temp_3/newDeviceConfig.java
+++ app/src/main/java/com/example/meetgoti/temp_3/newDeviceConfig.java
@@ -1,6 +1,5 @@
 package com.example.meetgoti.temp_3;
 
-import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
@@ -12,14 +11,18 @@ import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.meetgoti.temp_3.Bluetooth.Select;
 
-import me.aflak.bluetooth.Bluetooth;
+import com.example.meetgoti.temp_3.Bluetooth_Class.Bluetooth;
 
 /**
  * Created by meetgoti on 05/04/18.
@@ -31,6 +34,8 @@ public class newDeviceConfig extends AppCompatActivity implements Bluetooth.Comm
     private String name;
     private Bluetooth b;
     private boolean registered;
+    private ViewGroup my_linear_layout;
+    int button_id;
 
 
 
@@ -40,21 +45,30 @@ public class newDeviceConfig extends AppCompatActivity implements Bluetooth.Comm
         super.onCreate(savedInstanceState);
         setContentView(R.layout.newdeviceconfig);
 
+        button_id = 0;
         connection_state = findViewById(R.id.Connection_State);
         registered = false;
         b = new Bluetooth(this);
         b.enableBluetooth();
         b.setCommunicationCallback(this);
         int pos = getIntent().getExtras().getInt("pos");
+        my_linear_layout = (ViewGroup) findViewById(R.id.linear);
 
+        Log.d("Debug" , "Here");
         name = b.getPairedDevices().get(pos).getName().toString();
+
+        Log.d("Debug" , "Here");
         setTitle(name);
         Display("Connecting....");
+        Log.d("Debug" , "Here");
         b.connectToDevice(b.getPairedDevices().get(pos));
 
         IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
         registerReceiver(mReciever , filter);
         registered = true;
+        for(int i = 0 ; i < 17 ; i++)
+            add_button();
+        add_button();
     }
 
     private void Display(String s) {
@@ -127,7 +141,9 @@ public class newDeviceConfig extends AppCompatActivity implements Bluetooth.Comm
     @Override
     public void onConnect(BluetoothDevice device) {
         Display("Connected to " + device.getName()  + device.getAddress());
-        b.send("Welcome To Connection");
+        byte a = 5;
+        b.send_byte(a);
+
     }
 
     @Override
@@ -172,5 +188,22 @@ public class newDeviceConfig extends AppCompatActivity implements Bluetooth.Comm
         Intent i = new Intent(newDeviceConfig.this , Select.class);
         i.putExtra("Check","Check");
         startActivity(i);
+    }
+
+    public void add_button() {
+        View layout2 = LayoutInflater.from(this).inflate(R.layout.button_layout , my_linear_layout , false);
+        final Button btn2 = layout2.findViewById(R.id.but);
+        btn2.setText("WORLD");
+        btn2.setBackground(this.getResources().getDrawable(R.drawable.roundedbutton));
+        btn2.setId(button_id);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = view.getId();
+                btn2.setText("OK Pressed " + a);
+            }
+        });
+        button_id++;
+        my_linear_layout.addView(layout2);
     }
 }
